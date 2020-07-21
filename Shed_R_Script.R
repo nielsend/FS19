77f46ef0d53e6267e07afd16f205b6896d7420d4
Daniel Nielsen <nielsend13@gmail.com>
2020-07-21 08:32:07 -0500

    shed_R_script.R
    Shed R Script

---
+++ Shed_R_Script.R
@@ -0,0 +1,58 @@
+setwd('~/')
+
+install.packages("pracma")
+library(tidyverse)
+library(pracma)
+
+### Shedding data
+shed_data <- read_csv('~/FS19_ForAUCAnalysis.csv')
+
+
+sum_shed <- shed_data %>% group_by(Animal) %>%
+  summarise(AUC=trapz(Day, Shed),
+            AULC=trapz(Day, log(Shed+1)),
+            sum=sum(Shed), 
+            maxshed=Shed[which.max(Shed)], 
+            day_max=Day[which.max(Shed)], 
+            pos_samples=sum(Shed > 0), 
+            StudyVar=unique(StudyVar), 
+            Strain=unique(Strain))
+
+sum_shed %>%
+  ggplot(aes(x=Strain, y=AULC)) +
+  geom_boxplot()+
+  geom_boxplot(aes(
+   fill=StudyVar
+    ))+ ggtitle("FS19 Fecal Shedding AULC/Lineage") + xlab("Lineage")
+
+
+summary(aov(data = sum_shed, formula = AULC~Strain))
+aovDat <- aov(data = sum_shed, formula = AULC~Strain)
+TukeyHSD(aovDat)
+
+
+#### SWABS
+swab_data <- read_csv('~/RamsSwabs.csv')
+
+
+sum_swab <- swab_data %>% group_by(Animal) %>%
+  summarise(AUC=trapz(Day, Swab),
+            AULC=trapz(Day, log(Swab+1)),
+            sum=sum(Swab), 
+            maxswab=Swab[which.max(Swab)], 
+            day_max=Day[which.max(Swab)], 
+            pos_samples=sum(Swab > 0), 
+            StudyVar=unique(StudyVar), 
+            Strain=unique(Strain))
+
+sum_swab %>%
+  ggplot(aes(x=Strain, y=AULC)) +
+  geom_boxplot()+
+  geom_boxplot(aes(
+    fill=StudyVar
+  ))+ ggtitle("FS19 Fecal Swab AULC/Lineage") + xlab("Lineage")
+
+
+summary(aov(data = sum_swab, formula = AULC~Strain))
+aovDat <- aov(data = sum_swab, formula = AULC~Strain)
+TukeyHSD(aovDat)
